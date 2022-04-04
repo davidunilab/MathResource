0fb92a2ad863c29b374d6ec5c21ac51d6f2f1c83
David <davidchincharashvili@gmail.com>
2022-04-04 07:11:10 +0400

    add urls to init py

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def example():
-    return render_template('example.html')
+    return render_template('index.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/notfound')
+def not_found():
+    return render_template('not_found.html')
+
+@app.route('/auth')
+def auth():
+    return render_template('auth.html')
